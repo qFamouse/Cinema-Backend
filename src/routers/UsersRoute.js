8ffceccebfbef15ed17434e8a5c559fe7c3992d0
qFamouse <famouse_ru@mail.ru>
2021-12-12 15:04:59 +0300

    Delete test api

--- src/routers/UsersRoute.js
+++ src/routers/UsersRoute.js
@@ -8,8 +8,5 @@ router.get('/users/:id', usersController.GetById);
 router.post('/users', usersController.CreateOne);
 router.patch('/users/:id', usersController.EditById);
 router.delete('/users/:id', usersController.DeleteById);
-router.get('/test', function (req, res) {
-    res.send("Hello World");
-})
 
 module.exports= router;
