b6dae04dec33babf163f8508cc19d14a641619c7
qFamouse <famouse_ru@mail.ru>
2021-12-15 22:35:52 +0300

    Update User Validation

--- src/Schemes/UserScheme.js
+++ src/Schemes/UserScheme.js
@@ -1,6 +1,5 @@
 const Joi = require('joi');
 
-
 const UserScheme = {
     create: Joi.object().keys(
         {
@@ -28,6 +27,33 @@ const UserScheme = {
             email: Joi.string()
                 .email()
                 .required(),
+
+            phone: Joi.string(),
+        }
+    ),
+
+    edit: Joi.object().keys(
+        {
+            login: Joi.string()
+                .alphanum()
+                .min(3)
+                .max(30),
+
+            password: Joi.string()
+                .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+
+            roleId: Joi.number(),
+
+            avatar: Joi.string(),
+
+            firstName: Joi.string()
+                .min(3)
+                .max(30),
+
+            birthday: Joi.string(),
+
+            email: Joi.string()
+                .email(),
 
             phone: Joi.string(),
         }
--- src/routers/UsersRoute.js
+++ src/routers/UsersRoute.js
@@ -9,7 +9,7 @@ const usersController = require('../controllers/UsersController');
 router.get('/users', usersController.GetAll);
 router.get('/users/:id', usersController.GetById);
 router.post('/users', validate(UserScheme.create), usersController.CreateOne);
-router.patch('/users/:id', usersController.EditById);
+router.patch('/users/:id', validate(UserScheme.edit), usersController.EditById);
 router.delete('/users/:id', usersController.DeleteById);
 
 module.exports= router;
