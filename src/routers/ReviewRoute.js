92d192706dc80e906926e0f530fcaaed77ae1483
qFamouse <famouse_ru@mail.ru>
2021-12-19 22:32:27 +0300

    Reviews API Authorization

--- src/routers/ReviewRoute.js
+++ src/routers/ReviewRoute.js
@@ -4,8 +4,11 @@ const router = express.Router();
 const validate = require('../middleware/Validate');
 const reviewScheme = require('../schemes/ReviewScheme');
 
+const isAuthorize = require('../middleware/IsAuthorize');
+
 const reviewController = require('../controllers/ReviewController');
 
+router.use(isAuthorize);
 router.get('/', reviewController.GetAll);
 router.get('/:id', reviewController.GetDetailedById);
 router.post('/', validate(reviewScheme.create), reviewController.CreateOne);
