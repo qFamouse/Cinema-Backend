a27581bef74ec1aca6d0cab078ad8f325ac61d78
qFamouse <famouse_ru@mail.ru>
2021-12-30 18:08:53 +0300

    Modify ErrorHandler Adding handling for ValidationError (from sequelize)

--- src/middleware/ErrorHandler.js
+++ src/middleware/ErrorHandler.js
@@ -1,11 +1,17 @@
 const Response = require("../utils/Response");
 const HttpStatusCodeError = require("../Errors/HttpStatusCodeError");
+const ValidationError = require('sequelize').ValidationError;
 
 module.exports = (error, req, res, next) => {
+    console.log(error.name);
     if (error instanceof HttpStatusCodeError) {
         res.status(error.status || 500).json(new Response(error.message, error.status || 500));
     }
+    else if (error instanceof ValidationError) {
+        res.status(error.status || 400).json(new Response(error.errors[0].message, error.status || 400));
+    }
     else {
+        console.log(error);
         res.status(500).json(new Response("Internal Server Error", 500));
     }
 }
