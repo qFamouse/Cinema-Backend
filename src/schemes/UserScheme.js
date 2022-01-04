e9b1353ea55d25b809883a35b090b8e806512ff0
qFamouse <famouse_ru@mail.ru>
2022-01-04 20:40:20 +0300

    Add login validate scheme

--- src/schemes/UserScheme.js
+++ src/schemes/UserScheme.js
@@ -57,6 +57,19 @@ const UserScheme = {
 
             phone: Joi.string(),
         }
+    ),
+
+    login: Joi.object().keys(
+        {
+            login: Joi.string()
+                .alphanum()
+                .min(3)
+                .max(30)
+                .required(),
+
+            password: Joi.string()
+                .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+        }
     )
 };
 
