c1275887e55611ab70df6dcfd6109be23f9cf7f3
qFamouse <famouse_ru@mail.ru>
2022-01-05 01:29:20 +0300

    Create logs with mongoose

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
-
 // Solution to Try Catch Problem for Global Exception Handling
 const asyncRouter = require('async-express-router')
 asyncRouter(app);
+// Mongodb module
+const mongo = require('./mongo/Mongo');
+
 
 const loader = require('./loader');
 app.use(loader);
