d77c5f8a147d0c4fa0e89fa02c21d7a718577532
qFamouse <famouse_ru@mail.ru>
2022-01-04 20:39:52 +0300

    Connect Swagger

--- src/loader/index.js
+++ src/loader/index.js
@@ -1,9 +1,13 @@
 const Express = require('./Express');
 const Routing = require('./Routing');
 const ErrorHandler = require('../middleware/ErrorHandler');
+const Swagger = require('./Swagger');
 
+const swaggerUI = require('swagger-ui-express');
 const express = require('express');
 const router = express.Router();
+
+router.use('/api-docs', swaggerUI.serve, Swagger)
 
 router.use(Express);
 
