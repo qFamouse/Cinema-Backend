a648c44e7371c9b711e4e058d6bed2a1557f88ba
qFamouse <famouse_ru@mail.ru>
2022-01-06 18:23:15 +0300

    Complete Place Service Tests

--- tests/services/PlaceService.test.js
+++ tests/services/PlaceService.test.js
@@ -1,14 +1,26 @@
-const PlaceService = require('../../src/services/PlaceService');
+const PlaceServiceTest = require('../../src/services/PlaceService');
 
-jest.mock('PlaceRepository', () => {
-    return {
-        GetAll: jest.fn().mockImplementation(() => {
-            return 'dasdas';
+describe('PlaceService', () => {
+    describe('GetAll', () => {
+        test('should return array with all places', async () => {
+            expect(await PlaceServiceTest.GetAll()).not.toBeUndefined();
         })
-    }
-});
+    })
 
+    describe('Create', () => {
+        test('should catch error - place is occupied', async () => {
+            let place = {
+                "hallId": 2,
+                "floor": 1,
+                "seat": 1,
+                "row": 1,
+                "isVip": false
+            }
+            // Create place (catch if already exists)
+            try { await PlaceServiceTest.Create(place); } catch (e) {}
 
-test("KEK", async () => {
-    expect(await PlaceService.GetAll()).toBe('111')
-});
+            // test
+            await expect(PlaceServiceTest.Create(place)).rejects.toThrow();
+        })
+    })
+})
