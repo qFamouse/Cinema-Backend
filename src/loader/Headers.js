a3d07bebac16fdcf8f47651adf0a881837a761f1
Famouse <famouse_ru@mail.ru>
2022-05-30 20:23:50 +0300

    Adding CORS

--- src/loader/Headers.js
+++ src/loader/Headers.js
@@ -1,24 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const cors = require('cors');
 
-// It necessary for angular connection (we allowed connection from frontend)
-router.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+const corsOptions = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+    'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
+    'optionsSuccessStatus': 200
+}
+router.use('*', cors(corsOptions));
 
 module.exports = router;
