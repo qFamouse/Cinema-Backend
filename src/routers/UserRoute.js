cb023bb46b95d03869d858b5acb1d71cedc3c7e9
Famouse <famouse_ru@mail.ru>
2022-06-11 23:49:22 +0300

    Complete routers to get/set user avatar

--- src/routers/UserRoute.js
+++ src/routers/UserRoute.js
@@ -9,6 +9,29 @@ const isAuthorize = require('../middleware/IsAuthorize');
 const userController = require('../controllers/UserController');
 const mongoLogger = require("../utils/MongoLogger");
 
+const multer = require('multer');
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, callback) {
+            callback(null, '../uploads/avatars');
+        },
+        filename: function (req, file, callback) {
+            callback(null, Date.now() + file.originalname.substring(file.originalname.lastIndexOf('.')));
+        }
+    }),
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter(req, file, callback) {
+        if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+            callback(null, true);
+        }
+        else {
+            callback(null, false);
+        }
+    }
+});
+
 router.post('/login', validate(userScheme.login), userController.Login
     /*
     #swagger.tags = ['Users']
@@ -24,6 +47,8 @@ router.get('/', userController.GetAll
     #swagger.tags = ['Users']
     #swagger.security = [{ "bearerAuth": [] }]
     */);
+router.post('/avatar', upload.single('avatar'), userController.SetAvatarByCurrentUser)
+router.get('/avatar', userController.GetAvatarByCurrentUser)
 router.get('/user', userController.GetCurrentUser)
 router.patch('/update', validate(userScheme.edit), userController.UpdateCurrentUser)
 router.get('/:id', userController.GetById
