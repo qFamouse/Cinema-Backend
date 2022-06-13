ac2017f14c513bfbdc883d714bb24e2c7173157e
Famouse <famouse_ru@mail.ru>
2022-06-13 21:54:37 +0300

    After creating a seance will be automatically created tickets for this seance

--- src/services/SeanceService.js
+++ src/services/SeanceService.js
@@ -1,5 +1,7 @@
 const seanceRepository = require('../repositories/SeanceRepository');
 const hallRepository = require('../repositories/HallRepository');
+const placeRepository = require('../repositories/PlaceRepository');
+const ticketRepository = require('../repositories/TicketRepository');
 const {exist} = require("joi");
 const date = require('date-and-time')
 
@@ -10,19 +12,22 @@ class SeanceService {
 
     async GetRepertoire(from, to) {
         let repertoire = await seanceRepository.GetRepertoire(from, to);
-
         let halls = [];
         repertoire.forEach((seance) => {
             let hall = halls.find(hall => hall.id === seance.hallId);
 
             if (!hall) {
                 hall = seance.hall;
+                hall.rows = seance.hall.rows;
+                hall.seats = seance.hall.seats;
                 hall.seances = [];
                 halls.push(hall);
             }
 
-            seance.hallId = undefined;
+            // seance.hallId = undefined;
             seance.hall = undefined;
+
+
             // seance.movie.genre = seance.movie.genre.name;
             // seance.movie.country = seance.movie.country.name;
             // seance.date = date.format(seance.date,'YYYY-MM-DD HH:mm');
@@ -37,8 +42,22 @@ class SeanceService {
         return await seanceRepository.GetDetailedById(seanceId);
     }
 
-    async CreateOne(seance) {
-        return await seanceRepository.Create(seance);
+    async CreateOne(seanceData) {
+        let seance = await seanceRepository.Create(seanceData);
+        let hallPlaces = await placeRepository.GetPlacesByHallId(seance.hallId);
+
+        hallPlaces.forEach(place => {
+            let ticket = {
+                seanceId: seance.id,
+                placeId: place.id,
+                cost: parseFloat((Math.random() * (5 - 3) + 3).toFixed(1))
+                    + (place.isVip ? 3 : 0)
+            }
+
+            ticketRepository.Create(ticket)
+        })
+
+        return seance;
     }
 
     async EditById(seanceId, seance) {
